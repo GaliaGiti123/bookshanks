b81e41929a7f203ff7aeda5062c250d204a15fb9
Galia Rabbani <gitigalia@gmail.com>
2018-04-04 07:42:32 +0600

    template files defined

---
+++ header.php
@@ -0,0 +1,27 @@
+<?php include './dbconnect.php'?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>BookWorm - An online bookshop</title>
+	<link rel="stylesheet" type="text/css" href="./src/css/boipotro.css">
+</head>
+
+<body>
+	<div class="header">
+		<div class="logo"> </div>
+
+		<div class="nav">
+			<ul>
+				<li> <a href="#">Home</a></li>
+				<li><a href="#">Books</a></li>
+				<li><a href="#">Detail</a></li>
+				<li> <a href="#">About us</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="#">Sign up</a></li>
+				
+			</ul>
+		</div>
+
+	</div>
---
+++ register.php
@@ -0,0 +1,56 @@
+<?php include './header.php'?>
+
+
+<div class="book1 box"></div>
+<div class="book2 box"></div>
+<div class="book3 box"></div>
+<div class="book4 box"></div>
+
+<div class="hero">
+	<p class="heroText">Start reading anywhere..</p>
+	<div class="userAuth" id="userAuth">
+
+
+<form action="registrationSuccess.php">
+	<label>Username</label>
+	<br>
+	<input class="width100" type="text">
+	<br>
+	<br>
+	<label>Password</label>
+	<br>
+	<input class="width100" type="password">
+	
+	<br>
+	<br>
+	<label>Email</label>
+	<br>
+	<input class="width100" type="email">
+	<br>
+
+	<br>
+	<label>Address</label>
+	<br>
+	<input class="width100" type="text">
+	<br>
+	
+	<br>
+	<label>Phone</label>
+	<br>
+	<input class="width100" type="text">
+	<br>
+	
+	
+	
+	<br>
+	<br>
+	<input class="button buttonPrimary width100" type="submit" value="Sign up!">
+</form>
+<br>
+
+	</div>
+</div>
+
+
+
+<?php include './footer.php';?>
